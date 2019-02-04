2c1e633bfa26691b097fe54e0ac4f2dd3a71a1ca
Kevin.Fal <35910743+kevinfal@users.noreply.github.com>
2019-02-04 12:27:31 -0500

    Add files via upload

---
+++ Quadratic Sorting/src/Runner.java
@@ -0,0 +1,7 @@
+//Kevin Falconett
+public class Runner {
+	public static void main(String[] args){
+		Sorter s0 = new Sorter(10);
+		System.out.println(s0.getArr().length);
+	}
+}
--- Quadratic Sorting/src/Sorter.java
+++ Quadratic Sorting/src/Sorter.java
@@ -1,4 +1,29 @@
 //Kevin Falconetts
 public class Sorter {
-
+	
+	private int size;
+	
+	private int[] arr;
+	
+	public Sorter(int size){
+		this.size = size;
+		arr = new int[size];
+	}
+	
+	public int getSize(){
+		return size;
+	}
+	
+	public void setSize(int number){
+		size = number;
+	}
+	
+	public int[] getArr(){
+		return arr;
+	}
+	
+	public void setArr(int[] newArr){
+		arr = newArr;
+	}
+	
 }
