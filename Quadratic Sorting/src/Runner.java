da60973ce3c3f00970c82c1309a4ca43794c56be
Kevin.Fal <35910743+kevinfal@users.noreply.github.com>
2019-02-14 12:09:10 -0500

    Add files via upload

--- Quadratic Sorting/src/Runner.java
+++ Quadratic Sorting/src/Runner.java
@@ -3,60 +3,52 @@
 //uses bubble sort
 public class Runner {
 	public static void main(String[] args){
-		Sorter s0 = new Sorter(500);
-		
-		Sorter sorted = new Sorter(10);
-		System.out.println("Before Sort");
-		sorted.fillRandom();
-		sorted.printSorter();
-		System.out.println("After Sort");
-		sorted.sort();
-		sorted.printSorter();
-		System.out.println("Random");
-
-		s0.fillRandom();
-//		System.out.println("Before Sort");
-//		s0.printSorter();
-		s0.sort();
-//		System.out.println("After Sort");
-//		s0.printSorter();
-		System.out.println("Ascending");
-
+		Sorter s0 = new Sorter(10);
+		s0.testQuick();
+		s0.printSorter();
+		/*
 		Sorter s1 = new Sorter(5000);
-		s1.fillAscending();
-//		System.out.println("Before Sort");
-//		s1.printSorter();
-		s1.sort();
-//		System.out.println("After Sort");
-//		s1.printSorter();
+		s1.test();
 
-		System.out.println("Descending");
 		Sorter s2 = new Sorter(50000);
-		s2.fillDescending();
-//		System.out.println("Before Sort");
-//		s2.printSorter();
-		s2.sort();
-		//System.out.println("After Sort");
-		
-		
+		s2.test();
+		*/
 	}
 }
 /*
 Before Sort
-10 2 7 0 7 4 5 8 9 0 
-After Sort
-Comparisons: 45
-Swaps: 22
-0 0 2 4 5 7 7 8 9 10 
+Size: 500
 Random
 Comparisons: 124750
-Swaps: 61489
+Swaps: 63629
+Ascending
+Comparisons: 124750
+Swaps: 0
+Descending
+Comparisons: 124750
+Swaps: 124750
+
+Size: 5000
+Random
+Comparisons: 12497500
+Swaps: 6329045
 Ascending
 Comparisons: 12497500
 Swaps: 0
 Descending
+Comparisons: 12497500
+Swaps: 12497500
+
+Descending
+Size: 50000
+Random
+Comparisons: 1249975000
+Swaps: 627924180
+Ascending
+Comparisons: 1249975000
+Swaps: 0
+Descending
 Comparisons: 1249975000
 Swaps: 1249975000
-
  * 
  */
--- Quadratic Sorting/src/Sorter.java
+++ Quadratic Sorting/src/Sorter.java
@@ -52,6 +52,101 @@ public class Sorter {
 		}
 	
 	
+	public int median(int mid, int left, int right) {
+		//finds median of three
+		int[] medians = {mid,left,right};
+		
+		int highest = 0;
+		int lowest = right;
+		int med = 0;
+		
+		for(int x: medians){
+			
+			for(int i: medians){
+					
+				if(i >= highest){
+					highest = i;
+				}
+				else if(i <= lowest){
+					lowest = i;
+				}
+				else{
+					med = i;
+					}
+			}		
+		}
+		return med;
+	}
+
+	
+	public int[] quickSort(int[] list, int left, int right,int swaps, int comparisons) {
+		
+			
+			if(left>=right) {
+				return list;
+			}
+			
+			
+			int leftNum = list[left];
+			int rightNum = list[right];
+			int mid = list[(right+left)/2];
+			int pivot = median(mid,leftNum,rightNum);
+			
+			while(left<=right) {
+				
+				
+				
+				//if the element at left is less than pivot
+				if(list[left]<pivot) {
+					//pass it and add a comparison
+					comparisons++;
+					left++;
+				} 
+				
+				
+				//if element at right is greater than the pivot
+				else if(list[right]>pivot) {
+					//pass it and add comparison
+					comparisons++;
+					right--;
+				} 
+				//the elements should be right for swapping
+				else {
+					comparisons++;
+					swaps++;
+					int temp = list[left];
+					list[left]=list[right];
+					list[right]=temp;
+				}
+				
+				//if left and right are both equal to the pivot
+				if(list[left]==pivot && list[right]==pivot){
+					//pass
+					left++;
+					right--;
+				}
+				
+				
+			}
+			
+			quickSort(list,left, right, swaps, comparisons);
+						
+			System.out.println("Swaps: " +swaps);
+			System.out.println("Comparisons: " +comparisons);
+			return (quickSort(list,left,right, swaps, comparisons));
+	}
+	
+
+	
+	public void testQuick(){
+		fillDescending();
+		printSorter();
+		quickSort(arr, 0, size -1, 0, 0);
+		
+		
+	}
+	
+	
 	public void fillAscending(){
 		
 		
@@ -76,11 +171,35 @@ public class Sorter {
 		
 	}
 	
+	public void test(){
+		System.out.println("Size: " +size);
+		this.fillRandom();
+		System.out.println("Random");
+		this.sort();
+		System.out.println("Ascending");
+		this.fillAscending();
+		this.sort();
+		System.out.println("Descending");
+		this.fillDescending();
+		this.sort();
+		System.out.println("");
+	}
+	
+	public void printList(int[] arr){
+		
+		for(int i : arr){
+			System.out.print(i +" ");
+		}
+		System.out.println("");
+	}
+		
+	
+	
 	public void printSorter(){
 		for(int i : arr){
 			System.out.print(i +" ");
 		}
-		System.out.println("");;
+		System.out.println("");
 	}
 	
 }
